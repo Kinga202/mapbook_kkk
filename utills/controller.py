684fdf6e99e720eddc0eae97a28ed729a253444b
Kinga202 <kingakk998@gmail.com>
2025-04-14 09:20:29 +0200

    wip: new menu option: update user

--- utills/controller.py
+++ utills/controller.py
@@ -1,15 +1,26 @@
-def get_user_info(users_data:list[dict])->None:
+def get_user_info(users_data: list[dict]) -> None:
     for user in users_data:
         print(f'Twój znajomy: {user["name"]} z {user["location"]} opublikował {user["posts"]}')
 
+
 def add_user(users_data: list[dict]) -> None:
-    tmp_name:str = input('podaj imię: ')
-    tmp_location:str = input('podaj miejsce: ')
-    tmp_posts:int = int(input('podaj liczbe postów: '))
+    tmp_name: str = input('podaj imię: ')
+    tmp_location: str = input('podaj miejsce: ')
+    tmp_posts: int = int(input('podaj liczbe postów: '))
     users_data.append({'name': tmp_name, 'location': tmp_location, 'posts': tmp_posts})
 
-def remove_user(users_data: list[dict])->None:
-    user_name=input('Podaj imię użytkownika do usunięcia: ')
+
+def remove_user(users_data: list[dict]) -> None:
+    user_name = input('Podaj imię użytkownika do usunięcia: ')
     for user in users_data:
-        if user ['name']==user_name:
+        if user['name'] == user_name:
             users_data.remove(user)
+
+
+def update_user(users_data: list[dict]) -> None:
+    user_name = input('Podaj imię użytkownika do zmodyfikowania: ')
+    for user in users_data:
+        if user['name'] == user_name:
+            user['name'] = input('Podaj nowe imię: ')
+            user['location'] = input('Podaj nową lokalizacją: ')
+            user['posts'] = input('Podaj nową liczbe postów: ')
