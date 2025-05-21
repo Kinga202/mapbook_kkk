ce80634f3afefae8077a4e9821e55a01dd88c3b7
Kinga202 <kingakk998@gmail.com>
2025-05-21 08:42:07 +0200

    init: start umpty project

--- utills/model.py
+++
@@ -1,5 +0,0 @@
-users: list[dict] = [
-    {'name': 'Kinga', 'location': 'Warszawa', 'posts': 500, },
-    # {'name': 'Wika', 'location': 'Radzyń', 'posts': 400, },
-    # {'name': 'Daria', 'location': 'Łosice', 'posts': 200, }
-]
